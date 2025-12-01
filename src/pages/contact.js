7e5f882735d185d0f0e7e59e83ff35269b34b50e
Matthew Pick <matt@matthewpick.com>
2025-11-30 22:13:07 -0600

    use cloudfront

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,7 +20,7 @@ const ContactPage = ({
           <p>Let me help you kick start your next project &rarr;</p>
         </div>
         <div>
-          <form className="form-container" action="https://akvydh6jjc.execute-api.us-east-1.amazonaws.com/submit/matthewpick-com" method="post">
+          <form className="form-container" action="https://d3ul2m1fk3r35l.cloudfront.net/submit/matthewpick-com" method="post">
             <div>
               <label htmlFor="w3lName">Name</label>
               <input type="text" name="w3lName" id="w3lName"/>
