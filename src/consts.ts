afea7dd3bdd5d5df0f278f3f98f173558f070510
Matthew Pick <matt@matthewpick.com>
2026-01-19 00:58:21 -0600

    formatting

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = 'Matthew Pick';
-export const SITE_DESCRIPTION = 'Personal website and blog of Matthew Pick';
+export const SITE_DESCRIPTION = 'Midwest software engineer, taking the long view on fast-moving tech.';
 export const SITE_URL = 'https://matthewpick.com';
